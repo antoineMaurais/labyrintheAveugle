fafd306ca680f243f5d67628aadb7b5ae90f7454
Florian Sautejeau <florian.sautejeau@gmail.com>
2023-04-12 14:39:29 +0200

    creation d'un triangle à la place du carré pour le player

--- labyrinthe_2/src/Player.cpp
+++ labyrinthe_2/src/Player.cpp
@@ -36,35 +36,48 @@ void Player::checkWalls(const Maze &maze)
 {
     if (maze.isWall(x, y, prevX, prevY))
     {
-        //printf("Wall detected => retour(%d,%d)\n", prevX, prevY);
+        // printf("Wall detected => retour(%d,%d)\n", prevX, prevY);
         x = prevX;
         y = prevY;
     }
 }
 
+void filledTriangle(SDL_Renderer *renderer, int x1, int y1, int x2, int y2, int x3, int y3)
+{
+    SDL_RenderDrawLine(renderer, x1, y1, x2, y2);
+    SDL_RenderDrawLine(renderer, x2, y2, x3, y3);
+    SDL_RenderDrawLine(renderer, x3, y3, x1, y1);
+
+    int minX = std::min(std::min(x1, x2), x3);
+    int maxX = std::max(std::max(x1, x2), x3);
+    int minY = std::min(std::min(y1, y2), y3);
+    int maxY = std::max(std::max(y1, y2), y3);
+
+    for (int y = minY; y <= maxY; ++y)
+    {
+        for (int x = minX; x <= maxX; ++x)
+        {
+            int a = (x1 - x) * (y2 - y1) - (x2 - x1) * (y1 - y);
+            int b = (x2 - x) * (y3 - y2) - (x3 - x2) * (y2 - y);
+            int c = (x3 - x) * (y1 - y3) - (x1 - x3) * (y3 - y);
+
+            if ((a >= 0 && b >= 0 && c >= 0) || (a <= 0 && b <= 0 && c <= 0))
+            {
+                SDL_RenderDrawPoint(renderer, x, y);
+            }
+        }
+    }
+}
+
 void Player::draw(SDL_Renderer *renderer) const
 {
-    SDL_Rect playerRect;
-    /*int frontX = x * 40 + 20;
+    int frontX = x * 40 + 20;
     int frontY = y * 40 + 5;
     int backLeftX = x * 40 + 5;
-    int backLeftY = y * 40 + 35; 
+    int backLeftY = y * 40 + 35;
     int backRightX = x * 40 + 35;
     int backRightY = y * 40 + 35;
 
-    const std::vector<SDL_Vertex> verts =
-    {
-        { SDL_FPoint{ frontX, frontY }, SDL_Color{ 255, 0, 0, 255 }, SDL_FPoint{ 0 }, },
-        { SDL_FPoint{ backLeftX, backLeftY }, SDL_Color{ 255, 0, 0, 255 }, SDL_FPoint{ 0 }, },
-        { SDL_FPoint{ backRightX, backRightY }, SDL_Color{ 255, 0, 0, 255 }, SDL_FPoint{ 0 }, },
-    };*/
-
-    playerRect.x = x * 40 + 5;
-    playerRect.y = y * 40 + 5;
-    playerRect.w = 30;
-    playerRect.h = 30;
-
     SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255); // Red color
-    SDL_RenderFillRect(renderer, &playerRect);
-    //SDL_RenderGeometry( renderer, nullptr, verts.data(), verts.size(), nullptr, 0 );
+    filledTriangle(renderer, frontX, frontY, backLeftX, backLeftY, backRightX, backRightY);
 }
--- labyrinthe_2/src/build/CMakeFiles/labyrinth_project.dir/DependInfo.cmake
+++ labyrinthe_2/src/build/CMakeFiles/labyrinth_project.dir/DependInfo.cmake
@@ -4,10 +4,10 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "/home/rheriteau/Documents/ENSSAT/IMR2/contenu multimedia/labyrintheAveugle/labyrinthe_2/src/Maze.cpp" "/home/rheriteau/Documents/ENSSAT/IMR2/contenu multimedia/labyrintheAveugle/labyrinthe_2/src/build/CMakeFiles/labyrinth_project.dir/Maze.cpp.o"
-  "/home/rheriteau/Documents/ENSSAT/IMR2/contenu multimedia/labyrintheAveugle/labyrinthe_2/src/MazeGenerator.cpp" "/home/rheriteau/Documents/ENSSAT/IMR2/contenu multimedia/labyrintheAveugle/labyrinthe_2/src/build/CMakeFiles/labyrinth_project.dir/MazeGenerator.cpp.o"
-  "/home/rheriteau/Documents/ENSSAT/IMR2/contenu multimedia/labyrintheAveugle/labyrinthe_2/src/Player.cpp" "/home/rheriteau/Documents/ENSSAT/IMR2/contenu multimedia/labyrintheAveugle/labyrinthe_2/src/build/CMakeFiles/labyrinth_project.dir/Player.cpp.o"
-  "/home/rheriteau/Documents/ENSSAT/IMR2/contenu multimedia/labyrintheAveugle/labyrinthe_2/src/main.cpp" "/home/rheriteau/Documents/ENSSAT/IMR2/contenu multimedia/labyrintheAveugle/labyrinthe_2/src/build/CMakeFiles/labyrinth_project.dir/main.cpp.o"
+  "/home/florian/labyrintheAveugle/labyrinthe_2/src/Maze.cpp" "/home/florian/labyrintheAveugle/labyrinthe_2/src/build/CMakeFiles/labyrinth_project.dir/Maze.cpp.o"
+  "/home/florian/labyrintheAveugle/labyrinthe_2/src/MazeGenerator.cpp" "/home/florian/labyrintheAveugle/labyrinthe_2/src/build/CMakeFiles/labyrinth_project.dir/MazeGenerator.cpp.o"
+  "/home/florian/labyrintheAveugle/labyrinthe_2/src/Player.cpp" "/home/florian/labyrintheAveugle/labyrinthe_2/src/build/CMakeFiles/labyrinth_project.dir/Player.cpp.o"
+  "/home/florian/labyrintheAveugle/labyrinthe_2/src/main.cpp" "/home/florian/labyrintheAveugle/labyrinthe_2/src/build/CMakeFiles/labyrinth_project.dir/main.cpp.o"
   )
 set(CMAKE_CXX_COMPILER_ID "GNU")
 
